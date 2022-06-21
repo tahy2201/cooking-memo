37906b77b685c1bc1bc30152808e7da9063fc8bb
tahy2201 <hyg121arx8@gmail.com>
2022-06-21 23:58:05 +0900

    coomeをdocker化

--- schema/02_seeds.sql
+++ schema/02_seeds.sql
@@ -8,7 +8,7 @@ VALUES
 ('ごまだんご', 'pizza.jpg', 'それなりにあんこがはいったゴマ団子', 'tomato, cheese, dough', '{ "step1": "Put the dough in the oven", "step2": "Bake for 20 minutes" }', 2, '2022-05-01 00:00:00', '2020-05-03 00:00:00');
 
 INSERT INTO users
-    (logim_id, name, thumbnail, first_name, last_name, profile, create_date, update_date)
+    (login_id, name, thumbnail, first_name, last_name, profile, create_date, update_date)
 VALUES
 ('admin', 'th.jpeg', 'admin', 'admin_first', 'admin_last', 'admin_prof', '2020-01-01 00:00:00', '2020-01-01 00:00:00'),
 ('user', 'th.jpeg', 'user', 'user_first', 'user_last', 'user_prof', '2020-01-01 00:00:00', '2020-01-01 00:00:00');
