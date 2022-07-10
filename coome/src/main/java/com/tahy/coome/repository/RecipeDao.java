48a245c6cc821bef9c8eb8e81a0261c98174ddb7
tahy2201 <hyg121arx8@gmail.com>
2022-07-10 23:16:06 +0900

    getRecipe追加

--- coome/src/main/java/com/tahy/coome/repository/RecipeDao.java
+++ coome/src/main/java/com/tahy/coome/repository/RecipeDao.java
@@ -1,6 +1,7 @@
 package com.tahy.coome.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.tahy.coome.model.Recipe;
 
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RecipeDao extends JpaRepository<Recipe, Integer> {
 
     public List<Recipe> findByOwnerId(Integer ownerId);
+
+    public Optional<Recipe> findById(Integer id);
 }
