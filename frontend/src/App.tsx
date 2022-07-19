171ae769ff300175b6ac32ef875e0a93ecb9cc93
tahy2201 <hyg121arx8@gmail.com>
2022-07-19 23:11:19 +0900

    サイドバー構成変更、参照画面追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,30 +2,26 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { SampleHome, SamplePage1, SamplePage2 } from './components/Dashboard';
-import { MyRecipe } from './components/MyRecipe';
+import { RecipePage } from './components/RecipePage';
 import { RecipeList } from './components/RecipeList';
 import { DefaultPage } from './components/DefaultPage';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
-    <div className="App flex flex-row">
-      <div className="w-56 h-full bg-stone-200">
-        サイドバー
-      </div>
-      <div className="border">
-        <>
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<SampleHome />} />
-              <Route path="recipeList" element={<RecipeList />} />
-              <Route path="page1" element={<SamplePage1 />} />
-              <Route path="page2" element={<SamplePage2 />} />
-              <Route path="page3" element={<DefaultPage />} />
-            </Routes>
-          </BrowserRouter>
-        </>
-      </div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path="recipeList" element={<RecipeList />} />
+            <Route path="recipe/:id" element={<RecipePage />} />
+            <Route path="page1" element={<SamplePage1 />} />
+            <Route path="page2" element={<SamplePage2 />} />
+            <Route path="page3" element={<DefaultPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- frontend/src/components/MyRecipe.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const MyRecipe = () => {
-    return (
-        <h1>My Recipe</h1>
-    )
-}
